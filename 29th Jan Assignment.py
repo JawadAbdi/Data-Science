9c37558307ceec929a729a97cfb0a177e16e60cd
JawadAbdi <94121541+JawadAbdi@users.noreply.github.com>
2023-05-15 14:53:12 +0100

    Add files via upload

---
+++ 29th Jan Assignment.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1) Python was developed by Guido Van Rossum and launched in 1991.
+# 
+# 2) Python support Dynamic Semantics (Dynamic Typing & Dynamic Binding).
+# 
+# 3) Yes Python is case sensitive, as it treats uppercase and lowercase characters differently.
+# 
+# 4) Yhe correct extension of the Python file is .py.
+# 
+# 5) Python is an interpreted language.
+# 
+# 6) {}
+# 
+# 7) We use # to start a comment in Python.
+# 
+# 8) Using the sys.version method we can find the version of python that we are currently working on.
+# 
+# 9) Pyton supports the creation of anonymous function at runtime using a construct called Lambda.
+# 
+# 10) Pip is a package-management system written in Python and is used to install and manage software packages.
+# 
+# 11) Few built-in functions in Python:- map(), max(), list(), len(), input(), int().
+# 
+# 12) here is no limit on the length of the identifier name.
+# 
+# 13) Benefits of using python:-
+# -> Python is Extensible
+# -> Python Libraries
+# -> Portability
+# -> Productivity of Developers
+# 
+# 14) In Python memory allocation and deallocation method is automatic as developers created a garbage collector for python so that user does not have to do manual garbage collection.
+# 
+# 15) Install Python from Python.org and to add Python to your Windows PATH, you need to get its installation path. To do that, open up the Windows search bar and type python.exe (don't hit the Enter key). Then right-click on Python.exe that pops up in the resulting menu and select the Open file location option. In the Explorer windows that opens, click on the long directory bar to the left of the search bar. Highlight and copy the entire path text to your clipboard with Ctrl + c. Then continue with the next steps below. To add Python to the PATH in User variables, right-click on This PC, and select Properties. Once in the properties menu, click on the Advanced system settings option. In the next window, select the Advanced tab, and select Environment Variables. The Environment Variables menu has two distinct parts: an upper part called User variables, and a lower part named System variables. However, our focus is on the User variables in this case. Within the User variables menu, locate a variable called Path. Then paste the path you copied earlier in the Variable value option using Ctrl + v and click Ok.
+# 
+# 16) In python manual indentation is not required after defining a  loop or function it take indentation automatically.
+
+# In[ ]:
+
+
+
+
